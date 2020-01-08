bb4997701e851c0f1922e958a78c84dadf17597c
Yanniel Fandiño <yfandino@aryaly.com>
2020-01-08 20:53:52 +0100

    Refactor add form

--- src/components/AddForm/index.js
+++ src/components/AddForm/index.js
@@ -1,15 +1,16 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import { addPhonePart } from '../../redux/actions'
-import { brands, models } from './data';
+import { brands, models, stores } from './data';
 import './AddForm.css';
 
-const AddForm = ({ addPhonePart }) => {
+const AddForm = ({ addPhonePart, onClose }) => {
   const today = new Date();
   const tomorrow = new Date(today);
   tomorrow.setDate(tomorrow.getDate() + 1);
 
   const initState = {
+    store: "",
     nuuvola: "",
     code: "",
     brand: "",
@@ -33,55 +34,71 @@ const AddForm = ({ addPhonePart }) => {
   }
 
   return (
-    <div className="form-container">
-      <h2>Añadir nuevo</h2>
-      <form autoComplete="off" onSubmit={onSubmit}>
+    <div className="full-width">
+      <div className="form-container">
+        <h2>Añadir nuevo</h2>
+        <form autoComplete="off" onSubmit={onSubmit}>
         <div className="form-control">
-          <input id="nuuvola" type="text" value={state.nuuvola} onChange={onChange} />
-          <label htmlFor="nuuvola">Nº Orden Nuuvola</label>
-        </div>
-        <div className="form-control">
-          <input id="code" type="text" value={state.code} onChange={onChange} />
-          <label htmlFor="code">Código</label>
-        </div>
-        <div className="form-control">
-          <input id="brand" list="brandList" onChange={onChange} value={state.brand}/>
-          <label htmlFor="brand">Marca</label>
-          <datalist id="brandList">
-            { brands.map( 
-              (brand, i) => (
-                <option key={i} value={brand} />
-              )
-            )}
-          </datalist>
-        </div>
-        <div className="form-control">
-          <input id="model" list="modelList" onChange={onChange} value={state.model}/>
-          <label htmlFor="model">Modelo</label>
-          <datalist id="modelList">
-            { state.brand && models[state.brand] && (
-              models[state.brand].map( 
-                (model, i) => (
-                  <option key={i} value={model} />
+            <input id="store" list="storeList" onChange={onChange} value={state.store}/>
+            <label htmlFor="store">Marca</label>
+            <datalist id="storeList">
+              { stores.map( 
+                (store, i) => (
+                  <option key={i} value={store} />
                 )
-              )
-            )}
-          </datalist>
-        </div>
-        <div className="form-control">
-          <input id="description" type="text" value={state.description} onChange={onChange} />
-          <label htmlFor="description">Descripción</label>
-        </div>
-        <div className="form-control">
-          <input id="qty" type="number" value={state.qty} onChange={onChange} />
-          <label htmlFor="qty">Cantidad</label>
-        </div>
-        <div className="form-control">
-          <input id="date" type="date" value={state.date} onChange={onChange} />
-          <label htmlFor="date">Fecha cliente</label>
-        </div>
-        <button type="submit">Añadir</button>
-      </form>
+              )}
+            </datalist>
+          </div>
+          <div className="form-control">
+            <input id="nuuvola" type="text" value={state.nuuvola} onChange={onChange} />
+            <label htmlFor="nuuvola">Nº Orden Nuuvola</label>
+          </div>
+          <div className="form-control">
+            <input id="code" type="text" value={state.code} onChange={onChange} />
+            <label htmlFor="code">Código</label>
+          </div>
+          <div className="form-control">
+            <input id="brand" list="brandList" onChange={onChange} value={state.brand}/>
+            <label htmlFor="brand">Marca</label>
+            <datalist id="brandList">
+              { brands.map( 
+                (brand, i) => (
+                  <option key={i} value={brand} />
+                )
+              )}
+            </datalist>
+          </div>
+          <div className="form-control">
+            <input id="model" list="modelList" onChange={onChange} value={state.model}/>
+            <label htmlFor="model">Modelo</label>
+            <datalist id="modelList">
+              { state.brand && models[state.brand] && (
+                models[state.brand].map( 
+                  (model, i) => (
+                    <option key={i} value={model} />
+                  )
+                )
+              )}
+            </datalist>
+          </div>
+          <div className="form-control">
+            <input id="description" type="text" value={state.description} onChange={onChange} />
+            <label htmlFor="description">Descripción</label>
+          </div>
+          <div className="form-control">
+            <input id="qty" type="number" value={state.qty} onChange={onChange} />
+            <label htmlFor="qty">Cantidad</label>
+          </div>
+          <div className="form-control">
+            <input id="date" type="date" value={state.date} onChange={onChange} />
+            <label htmlFor="date">Fecha cliente</label>
+          </div>
+          <div className="form-buttons">
+            <button className="btn" type="submit">Añadir</button>
+            <button className="btn ghost" type="reset" onClick={onClose}>Cancel</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
